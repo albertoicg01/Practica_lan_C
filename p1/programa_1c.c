33f9cb7f7eb4712cddc9685cbdd4365cca708d32
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2021-05-23 21:56:57 -0500

    Problema resuelto pero no optimizado

--- p1/programa_1c.c
+++ p1/programa_1c.c
@@ -8,26 +8,38 @@ DESC    : A simple C program.
 #include<stdio.h>
 int main(){
     //Type your code here
-    float sum=0, num=0,counter=1;
+    float sum=0, num=0,subjects=0;
     
     printf("----------------------------------\n");
     printf("Programa que calcula promedios\n");
-    printf("Presiona -1 para terminar y arrojar resultado\n");
+    printf("Si ingresas -1, la ejecucion se detendra\n");
     printf("----------------------------------\n\n");
     
     do
     {   
 
-        printf("Introduce tu calificacion de la materia %f: ",counter);    
+        printf("Introduce tu calificacion de la materia %f: ",(subjects+1));    
         scanf("%f",&num);
-        sum+=num;
-        counter+=1;
+
+        //Excluye la bandera para terminar el programa        
+        if (num!=-1)
+        {
+            //Acomulador
+            sum+=num;
+
+            //Contador de materias
+            subjects+=1;
+            
+        }
+        
 
 
     } while (num!=-1);
-    printf("\t\n\tCounter es: %f \n",counter);
-    printf("\t\n\tnum es: %f \n",counter);
-    printf("\t\n\tTu promedio es: %f \n",(sum/counter));
+
+
+    printf("\t\n\tsubjects es: %f \n",subjects);
+    printf("\t\n\tsum es: %f \n",sum);
+    printf("\t\n\tTu promedio es: %f \n",(sum/subjects));
 
     return 0;
 }
